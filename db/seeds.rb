0047d4cd6cb1181c05a093bf8c4fd0b4ccff1002
IbrahimCh <ibrahimchm@gmail.com>
2023-06-06 22:18:58 -0400

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ end
 # Crear clientes de muestra y asignar películas arrendadas
 10.times do
   cliente = Cliente.create(nombre: Faker::Name.name)
-  peliculas_arrendadas = Pelicula.all.sample(3)  # Seleccionar 3 películas al azar como películas arrendadas
+  peliculas_arrendadas = Pelicula.all.sample(4)  # Seleccionar 3 películas al azar como películas arrendadas
   peliculas_arrendadas.each do |pelicula|
     cliente.arriendo_peliculas.create(pelicula: pelicula)
   end
