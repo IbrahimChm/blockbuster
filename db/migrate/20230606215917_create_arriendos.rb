6ba6e84e9c170e145ba1fb26e6fdf293b389c851
IbrahimCh <ibrahimchm@gmail.com>
2023-06-07 00:28:58 -0400

    on delete cascade

--- db/migrate/20230606215917_create_arriendos.rb
+++ db/migrate/20230606215917_create_arriendos.rb
@@ -1,10 +1,11 @@
 class CreateArriendos < ActiveRecord::Migration[7.0]
   def change
     create_table :arriendos do |t|
-      t.references :cliente, null: false, foreign_key: true
-      t.references :pelicula, null: false, foreign_key: true
+      t.references :cliente, null: false, foreign_key: { on_delete: :cascade }
+      t.references :pelicula, null: false, foreign_key: { on_delete: :cascade }
 
       t.timestamps
     end
   end
 end
+
