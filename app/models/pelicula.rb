8d3b1fc8fb6bfb7e6e7ded306ffb7fbf74f5d47e
IbrahimCh <ibrahimchm@gmail.com>
2023-06-07 00:59:10 -0400

    pelicula

--- app/models/pelicula.rb
+++ app/models/pelicula.rb
@@ -1,7 +1,8 @@
 class Pelicula < ApplicationRecord
-  belongs_to :arriendo, optional: true
+  belongs_to :cliente, optional: true
   has_many :arriendos, dependent: :destroy
   has_many :clientes, through: :arriendos
   
   validates :titulo, presence: true
 end
+
